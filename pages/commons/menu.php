995887191b0c8ef3e9e18377254bc1762a0b65e6
Masamune42 <a.caignard@hotmail.fr>
2020-03-13 09:01:19 +0100

    Mise en place du carrousel

--- pages/commons/menu.php
+++ pages/commons/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light perso_bgBlue">
+<nav class="navbar navbar-expand-xl navbar-light perso_bgBlue">
     <a class="navbar-brand text-white" href="#">Alexandre CAIGNARD</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@
                 <a class="nav-link text-white" href="#">Présentation</a>
             </li>   
             <li class="nav-item mx-2">
-                <a class="nav-link text-white" href="#">Mon CV</a>
+                <a class="nav-link text-white" href="#">CV</a>
             </li>   
             <li class="nav-item mx-2">
                 <a class="nav-link text-white" href="#">Compétences</a>
--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,35 @@
 <?php ob_start(); ?>
 
-<h1>Page d'accueil</h1>
-<button type="button" class="btn btn-success">Valider</button>
+<div id="carouselExampleIndicators" class="carousel slide perso_bgBlueDegrede" data-ride="carousel">
+    <!-- indicateurs, rectangles en bas du carrousel -->
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" class="bg-dark" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" class="bg-dark" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" class="bg-dark" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <!-- w-75 : width = 75%, h-75 : height = 75% -->
+        <!-- mx-auto : margin auto pour centrer -->
+        <!-- img-thumbnail : ajoute un effet arrondi aux images -->
+            <img src="../sources/images/cours1.png" class="d-block w-75 h-75 mx-auto img-thumbnail border border-dark" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../sources/images/cours3.png" class="d-block w-75 h-75 mx-auto img-thumbnail border border-dark" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../sources/images/catalogue.png" class="d-block w-75 h-75 mx-auto img-thumbnail border border-dark" alt="...">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 
 <?php
 $content = ob_get_clean();
