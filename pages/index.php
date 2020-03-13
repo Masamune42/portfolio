1c82d9a59bc30a0307c7ebfbccfd60fd1949c7eb
Masamune42 <a.caignard@hotmail.fr>
2020-03-13 15:11:55 +0100

    Ajout de plusieurs cards et placement dans un card-group pour les grouper

--- pages/index.php
+++ pages/index.php
@@ -82,14 +82,97 @@
 
 <div class="container text-center" id="portfolio">
     <h2 class="m-5 perso_colorBlueLight">Mon Portfolio</h2>
-    <div class="card border-success mb-3" style="max-width: 18rem;">
-        <div class="card-header bg-transparent border-success">Cours JavaScript</div>
-        <div class="card-body text-success p-0">
-            <!-- w-100 : taille de 100% dans le div -->
-            <img src="../sources/images/cours1.png" class="d-block w-100">
+    <div class="card-group">
+        <div class="card border-success mb-3">
+            <div class="card-header bg-transparent border-success">Cours JavaScript</div>
+            <div class="card-body text-success p-0">
+                <!-- w-100 : taille de 100% dans le div -->
+                <img src="../sources/images/cours1.png" class="d-block w-100">
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cours1">En savoir plus</button>
+            </div>
         </div>
-        <div class="card-footer bg-transparent border-success">
-            <button type="button" class="btn btn-primary">En savoir plus</button>
+        <div class="card border-success mb-3">
+            <div class="card-header bg-transparent border-success">Cours PHP</div>
+            <div class="card-body text-success p-0">
+                <!-- w-100 : taille de 100% dans le div -->
+                <img src="../sources/images/cours3.png" class="d-block w-100">
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cours3">En savoir plus</button>
+            </div>
+        </div>
+        <div class="card border-success mb-3">
+            <div class="card-header bg-transparent border-success">Cas pratique PHP / MYSQL</div>
+            <div class="card-body text-success p-0">
+                <!-- w-100 : taille de 100% dans le div -->
+                <img src="../sources/images/catalogue.png" class="d-block w-100">
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#catalogue">En savoir plus</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="cours1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cours JavaScript</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="../sources/images/cours1.png" class="d-block w-100">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente rem doloribus asperiores eum nobis quaerat laboriosam sequi aut optio corrupti aliquid voluptatum voluptas, quam veritatis dolorem excepturi possimus fugiat accusantium!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="cours3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cours PHP</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="../sources/images/cours3.png" class="d-block w-100">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente rem doloribus asperiores eum nobis quaerat laboriosam sequi aut optio corrupti aliquid voluptatum voluptas, quam veritatis dolorem excepturi possimus fugiat accusantium!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="catalogue" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cas pratique PHP / MYSQL</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="../sources/images/catalogue.png" class="d-block w-100">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente rem doloribus asperiores eum nobis quaerat laboriosam sequi aut optio corrupti aliquid voluptatum voluptas, quam veritatis dolorem excepturi possimus fugiat accusantium!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+            </div>
         </div>
     </div>
 </div>
