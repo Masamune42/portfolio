b36a6c6f044360f41ac017e04114b9569fc3cd46
alex <a.caignard@hotmail.fr>
2020-03-15 11:20:14 +0100

    Formulaire de contact avec envoi de mail

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,18 @@
 <?php ob_start(); ?>
 
+<?php
+if (isset($_POST['mail']) && isset($_POST['objet']) && isset($_POST['message'])) {
+    $to = "test@test.fr";
+    $from = isset($_POST['mail']);
+    $object = isset($_POST['objet']);
+    $message = isset($_POST['message']);
+    mail($to, $object, $message, $from);
+?>
+    <div class="alert alert-success" role="alert">
+        Message envoyé !
+    </div>
+<?php } ?>
+
 <div class="container text-center" id="contact">
     <!-- marge extérieur de 5 -->
     <h2 class="m-5 perso_colorBlueLight">Mes informations personnelles</h2>
@@ -21,6 +34,29 @@
             </tr>
         </tbody>
     </table>
+</div>
+
+<div class="container text-center">
+    <h2 class="m-5 perso_colorBlueLight">Formulaire de contact</h2>
+    <form method="POST" action="">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Adresse mail :</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" name="mail" placeholder="Entrez votre mail">
+        </div>
+        <div class="form-group">
+            <label for="objet">Objet :</label>
+            <select class="form-control" id="objet" name="objet">
+                <option value="question">Question</option>
+                <option value="remarque">Remarque</option>
+                <option value="autre">Autre</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="message">Message :</label>
+            <textarea class="form-control" id="message" rows="3" name="message" placeholder="Votre message..."></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Valider</button>
+    </form>
 </div>
 <?php
 $content = ob_get_clean();
